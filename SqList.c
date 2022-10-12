f163008a8ebdc4a5b62ac1ba267eb876fb0911d8
hang245141253 <hang245141253@gmail.com>
2022-10-12 14:49:54 +0800

    main

--- SqList.c
+++ SqList.c
@@ -18,14 +18,53 @@ typedef struct {
     int length;
 } SqList;
 // *在定义指针，声明指针时靠近前方，靠近后方时理解为解引用
+
+// 算法2.1 顺序表的初始化
 Status InitList(SqList* L) {
     L->elem = (ElemType*)malloc(sizeof(ElemType) * LIST_INIT_SIZE);
     if (L->elem == NULL) {
         exit(OVERFLOW);
     }
     L->length = 0;      //未解指针，需要->指向
-    (*L).listsize = 0; //先解指针，再用.访问。（与上等价）
+    (*L).listsize = LIST_INIT_SIZE; //先解指针，再用.访问。（与上等价）
     printf("Initial!\n");
+    return OK;
+}
+
+// 算法2.2 顺序表的取值
+Status GetElem(SqList L, int i, ElemType* e) {
+    //i的含义是位序，所以i∈[1, length]
+    if (i < 1 || i > L.length) {
+        return ERROR;   
+    }
+    *e = L.elem[i - 1]; //数组下标j = 位序i - 1
+    printf("GetElem %d at LOC(%d)\n", *e, i);
+    return OK;
+}
+
+// 算法2.3 查找
+int LocateElem(SqList L, ElemType e) {
+    for (int j = 0; j < L.length; j++) {
+        if (L.elem[j] == e) {
+            return j + 1;   // 位序i = 数组下标j + 1
+        }
+    }
+    return 0;   //查找失败
+}// 因为返回值有非0、1的值，所以用int 不用Status
+
+// 算法2.4 插入
+Status ListInsert(SqList L, int i, ElemType e) {
+    if (i < 1 || i > L.length) {
+        return ERROR;   //i∈[i, length]
+    }
+    if (L.length >= L.listsize) {
+        return ERROR; // or 扩容
+    }
+    for (int j = L.length - 1; j >= i - 1; j--) { //记j为数组下标
+        L.elem[j + 1] = L.elem[j];  //插入位置及之后元素后移
+    }
+    L.elem[i - 1] = e;  //在位置i插入元素 (记i - 1为数组下标)
+    L.length++;         //表长+1
     return OK;
 }
 
