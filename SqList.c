836e8e424e15ab546af4811d6c92d532fc168eac
hang245141253 <hang245141253@gmail.com>
2022-10-12 17:23:39 +0800

    main

--- SqList.c
+++ SqList.c
@@ -1,4 +1,4 @@
-/* #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #define OK 1
 #define ERROR 0
@@ -54,8 +54,8 @@ int LocateElem(SqList L, ElemType e) {
 
 // 算法2.4 顺序表的插入
 Status ListInsert(SqList* L, int i, ElemType e) {
-    if (i < 1 || i > L->length + 1) {   //length + 1则可以直接插入尾部 
-        return ERROR;  // i∈[i, length + 1]
+    if (i < 1 || i > L->length + 1) {  // length + 1则可以直接插入尾部
+        return ERROR;                  // i∈[i, length + 1]
     }
     if (L->length >= L->listsize) {
         return ERROR;  // or 扩容
@@ -134,4 +134,4 @@ int main() {
     }
 
     return 0;
-} */
+}
