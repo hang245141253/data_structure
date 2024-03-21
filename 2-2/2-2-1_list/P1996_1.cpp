d0722b58d1a2729f21bff6d961fc30ac02bae156
hang245141253 <hang245141253@gmail.com>
2024-03-21 17:54:57 +0800

    2024/3/21

---
+++ 2-2/2-2-1_list/P1996_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node *cur, *prev; 
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    // 建立链表
+    Node* head = new Node;
+    head->data = 1, head->next = nullptr;
+
+    cur = head;
+    for (int i = 2; i < n; i++) {
+        Node* p = new Node;
+        p->data = i, p->next = nullptr;
+        cur->next = p;
+        cur = cur->next; // 或者 cur = p
+    }
+
+    // debug遍历
+    for (Node* i = head; i != nullptr; i = i->next) {
+        cout << i->data << " ";
+    }
+
+    cur = head;
+
+    return 0;
+}
