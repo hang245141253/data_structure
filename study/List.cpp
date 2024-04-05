51f86fc8f0ec3a5d7eb5f48b12134d8ca88fa6ac
hang245141253 <hang245141253@gmail.com>
2024-04-06 02:13:07 +0800

    2024/4/6

--- study/List.cpp
+++ study/List.cpp
@@ -8,6 +8,22 @@ struct Node {
     Node* next;
 };
 
+void reverse(Node *p) {
+    if (p == nullptr) return;
+    reverse(p->next);
+    printf("%d->", p->data);
+    return;
+}
+
+// 扩展反转链表:反转前n个结点
+Node* reverse(Node *head, int n) {
+    if (n == 1) return head;
+    Node *tail = head->next, *new_head = reverse(head->next, n - 1); 
+    head->next = tail->next; // 这里不能写成head->next = nullptr,因为不是尾插
+    tail->next = head;
+    return new_head;
+}
+
 int main() {
     Node* head = NULL;
     head = new Node(1);
@@ -15,12 +31,20 @@ int main() {
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
 
+    // 通过递归回溯反转
+    reverse(head), printf("\n");
+
+    // 反转前n个
+    head = reverse(head, 3);
+
     // 遍历
     Node* p = head;
     while (p != nullptr) {
         printf("%d->", p->data);
         p = p->next;
     } printf("\n");
+
+    
 
     return 0;
 }
