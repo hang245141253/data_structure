7e4aa4a3cca272ff0aa67f17c55d342e91dfb5b8
hang245141253 <hang245141253@gmail.com>
2024-05-23 19:52:57 +0800

    2024/5/23

---
+++ mentu/day5/heap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 1000
+int data[MAX_N + 5], cnt = 0; // cnt记录当前堆中元素数量
+
+void push(int x) {
+    
+}
+
+int main() {
+    
+    return 0;
+}
