b5acb67fb47e26a4e9988969b0a518099af51d88
hang245141253 <hang245141253@gmail.com>
2024-07-05 14:48:24 +0800

    2024/7/5

--- 1.cpp
+++ 1.cpp
@@ -1,55 +1,36 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <functional>
+#include<bits/stdc++.h>
 using namespace std;
+const int N=1e5+5;
+int A[N], cnt;
 
-class Solution {
-public:
-    template<typename T>
-    class heap : public vector<T> {  // 继承vector<T>的公共接口
-    public:
-        // 模板(构造)函数
-        template<typename Func_T>
-        heap(Func_T cmp) : cmp(cmp) {} 
+int build(int l, int r) {
+    if (l > r) return 0;     
+    // if (l == r) return 1;    
+    int pos = l;             
+    cnt++;                  
 
-        void push(const T &a) { // &引用而不是拷贝，节约开销，const防止a的值被修改
-            this->push_back(a);
-            push_heap(this->begin(), this->end(), cmp);
+    for (int i = l + 1; i <= r; i++) {
+        cnt++;                
+        if (A[i] > A[pos]) { 
+            pos = i;       
         }
-
-        void pop() {
-            pop_heap(this->begin(), this->end(), cmp);
-            this->pop_back();
-        }
-
-        T &top() { // &引用，可访问、修改堆顶。不加&只能访问一个拷贝值，不能修改
-            return this->at(0);
-        }
-
-    // private:
-        function<bool(T, T)> cmp; // 相当于函数指针
-    };
-
-    // 手动写比较规则，大顶堆是小于号
-    struct CMP {  
-        bool operator()(vector<int> a, vector<int> b) {
-            return a[0] + a[1] < b[0] + b[1];
-        }
-    };
-
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        // 堆中的元素是长度为2的vector，数组中的两位分别代表数组中的第一个值和第二个值
-        heap<vector<int>> h{CMP()};
-        vector<int> tmp(2);
-        for (auto x : nums1) {
-            for (auto y : nums2) {
-                tmp[0] = x, tmp[1] = y;
-                h.push(tmp);
-                if (h.size() > k) h.pop();
-            }
-        }
-        return h;
     }
-};
+
+
+    int ls = build(l, pos-1);
+
+    int rs = build(pos+1, r);
+
+    return max(ls, rs) + 1;  
+}
+
+int main() {
+    int n;
+    cin >> n;  
+    for(int i = 1; i <= n; ++i) {
+        cin >> A[i];  
+    }
+    cnt = 0;  
+    int dep = build(1, n); 
+    cout << dep << " " << cnt << endl;  
+}
