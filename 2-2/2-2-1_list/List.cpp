41ee300efd3feadf56a8d01590f8efaefa587790
hang245141253 <hang245141253@gmail.com>
2025-03-02 18:43:45 +0800

    2025/3/2

--- 2-2/2-2-1_list/List.cpp
+++ 2-2/2-2-1_list/List.cpp
@@ -1,257 +1,213 @@
 #include <iostream>
-#include <list>
 using namespace std;
 
-typedef int ElemType;  // 数据元素类型约定为ElemType, 由用户在使用该数据类型时自行定义
+typedef int ElemType;  // 数据元素类型
 
-/*
-为什么要有链表的存在?（相比较顺序表的优势而言）
-    线性存储结构在进行中间插入和删除操作时需移动大量元素，浪费资源.
-    所以考虑链式存储的线性表：即 链表
-
-
-*/
-
-/****************************** 结构定义 ************************************/
-
-// 结点，只有后继，是单向链表
+// 单链表节点定义
 struct ListNode {
-    ElemType data;
-    struct ListNode* next;
+    ElemType val;      // 节点存储的值
+    ListNode* next;    // 指向下一个节点的指针
+    // 构造函数，初始化节点值为x，next为nullptr
+    ListNode(ElemType x) : val(x), next(nullptr) {}
 };
 
-// 链表
-struct List {
-    struct ListNode* head;
-    struct ListNode* tail;
-    size_t size;
-};
-
-// 初始化链表
-void initList(List& list) {
-    list.head = list.tail = NULL;
-    list.size = 0;
+// 在链表头插入节点
+void push_front(ListNode*& head, ElemType value) {
+    ListNode* new_node = new ListNode(value);  // 创建新节点
+    new_node->next = head;  // 新节点的next指向当前头节点
+    head = new_node;  // 更新头节点为新节点
 }
 
-// 创建节点
-ListNode* createNode(int value) {
-    ListNode* newNode = new ListNode;
-    if (newNode == nullptr) {
-        // 处理内存分配失败的情况
-        cerr << "Memory allocation failed" << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    newNode->data = value;
-    newNode->next = nullptr;
-
-    return newNode;
-}
-
-// 在链表头部插入元素
-void pushFront(List& list, int value) {
-    ListNode* newNode = createNode(value);
-
-    if (list.head == nullptr) {
-        list.head = list.tail = newNode;
+// 在链表尾插入节点
+void push_back(ListNode*& head, ElemType value) {
+    ListNode* new_node = new ListNode(value);  // 创建新节点
+    if (!head) {  // 如果链表为空，直接将新节点作为头节点
+        head = new_node;
     } else {
-        newNode->next = list.head;
-        list.head = newNode;
+        ListNode* cur = head;
+        while (cur->next) {  // 遍历链表到达尾部
+            cur = cur->next;
+        }
+        cur->next = new_node;  // 将新节点连接到链表尾部
     }
-
-    list.size++;
 }
 
-// 在链表尾部插入元素
-void pushBack(List& list, int value) {
-    ListNode* newNode = createNode(value);
-
-    if (list.tail == nullptr) {  // 如果链表为空，那么这个结点既是头部也是尾部
-        list.head = list.tail = newNode;
-    } else {
-        list.tail->next = newNode;
-        list.tail = newNode;
-    }
-
-    list.size++;
-}
-
-// 插入元素到指定位置
-/*
-size_t 是 C/C++ 中的一种无符号整数类型，
-通常用于表示对象的大小或容器的大小。
-它是根据系统架构的不同而有所变化，但它总是足够大以容纳系统中最大可能的对象大小。
-*/
-void insert(List& list, size_t position, int value) {
-    if (position > list.size) {
-        cerr << "Invalid position for insertion" << endl;
+// 在指定位置插入节点
+void insert(ListNode*& head, size_t index, ElemType value) {
+    if (index == 0) {
+        push_front(head, value);  // 如果插入位置是0，调用push_front插入
         return;
     }
 
-    if (position == 0) {
-        pushFront(list, value);
-    } else if (position == list.size) {
-        pushBack(list, value);
-    } else {
-        ListNode* newNode = createNode(value);
-        ListNode* current = list.head;
-        
-        // 遍历n - 1次, 到要插入位置前一个结点处
-        for (size_t i = 1; i < position; ++i) {
-            current = current->next;
+    ListNode* cur = head;
+    for (size_t i = 1; i < index; ++i) {  // 遍历找到目标位置的前一个节点
+        if (!cur) {  // 如果当前位置超出了链表的长度
+            cout << "Index out of bounds!" << endl;
+            return;
         }
-
-        newNode->next = current->next;
-        current->next = newNode;
-
-        list.size++;
+        cur = cur->next;
     }
+
+    ListNode* new_node = new ListNode(value);  // 创建新节点
+    new_node->next = cur->next;  // 新节点的next指向原来节点的next
+    cur->next = new_node;  // 将前一个节点的next指向新节点
 }
 
-// 删除链表头部元素
-void popFront(List& list) {
-    if (list.head != nullptr) {
-        ListNode* temp = list.head;
-        list.head = list.head->next;
-        delete temp;
-
-        if (list.head == nullptr) {
-            list.tail = nullptr;
-        }
-
-        list.size--;
+// 删除链表头节点
+void pop_front(ListNode*& head) {
+    if (!head) {  // 如果链表为空
+        cout << "List is empty!" << endl;
+        return;
     }
+    ListNode* temp = head;  // 暂存头节点
+    head = head->next;  // 更新头节点为下一个节点
+    delete temp;  // 删除原头节点
 }
 
-// 删除链表尾部元素
-void popBack(List& list) {
-    if (list.head != nullptr) {
-        ListNode* temp = list.head;
-        ListNode* prev = nullptr;
-
-        // 单项链表只能从头找删除的前一个位置
-        while (temp->next != nullptr) {
-            prev = temp;
-            temp = temp->next;
-        }
-
-        delete temp;
-
-        if (prev == nullptr) {
-            list.head = list.tail = nullptr;
-        } else {
-            prev->next = nullptr;
-            list.tail = prev;
-        }
-
-        list.size--;
+// 删除链表尾节点
+void pop_back(ListNode*& head) {
+    if (!head) {  // 如果链表为空
+        cout << "List is empty!" << endl;
+        return;
     }
-}
-
-// 删除链表中特定位置的节点
-void erase(List& list, size_t position) {
-    if (position >= list.size) {
-        cerr << "Invalid position for erasing" << endl;
+    if (!head->next) {  // 如果链表只有一个节点
+        delete head;  // 删除头节点
+        head = nullptr;  // 头节点变为空
         return;
     }
 
-    if (position == 0) {
-        popFront(list);
-    } else if (position == list.size - 1) {
-        popBack(list);
+    ListNode* cur = head;
+    while (cur->next->next) {  // 遍历到倒数第二个节点
+        cur = cur->next;
+    }
+    delete cur->next;  // 删除尾节点
+    cur->next = nullptr;  // 更新倒数第二个节点的next为nullptr
+}
+
+// 删除指定位置的节点
+void erase(ListNode*& head, size_t index) {
+    if (index == 0) {
+        pop_front(head);  // 如果是删除头节点，调用pop_front
+        return;
+    }
+
+    ListNode* cur = head;
+    for (size_t i = 1; i < index; ++i) {  // 遍历找到目标位置的前一个节点
+        if (!cur) {
+            cout << "Index out of bounds!" << endl;
+            return;
+        }
+        cur = cur->next;
+    }
+
+    ListNode* temp = cur->next;  // 暂存目标节点
+    if (temp) {
+        cur->next = temp->next;  // 前一个节点的next指向目标节点的next
+        delete temp;  // 删除目标节点
     } else {
-        ListNode* current = list.head;  // 头结点的位置是0
-        ListNode* prev = nullptr;       // 当前位置的前一个结点
+        cout << "Index out of bounds!" << endl;
+    }
+}
 
-        // 遍历n次, 到要删除位置结点处
-        for (size_t i = 0; i < position; ++i) {
-            prev = current;
-            current = current->next;
+// 修改指定位置的节点值
+void set(ListNode* &head, size_t index, ElemType value) {
+    ListNode* cur = head;
+    for (size_t i = 0; i < index; ++i) {  // 遍历找到目标位置的节点
+        if (!cur) {
+            cout << "Index out of bounds!" << endl;
+            return;
         }
-
-        prev->next = current->next;
-        delete current;
-
-        list.size--;
+        cur = cur->next;
     }
+    cur->val = value;  // 修改节点值
 }
 
-ListNode* findElem(List& list, ElemType value) {
-    ListNode* current = list.head;
+// 获取链表头节点的值
+ElemType front(ListNode* head) {
+    if (!head) {  // 如果链表为空
+        cout << "List is empty!" << endl;
+        return ElemType();  // 返回默认值0
+    }
+    return head->val;  // 返回头节点的值
+}
 
-    while (current != nullptr) {
-        if (current->data == value) {
-            return current;  // 返回找到的节点的指针
+// 获取链表尾节点的值
+ElemType back(ListNode* head) {
+    if (!head) {  // 如果链表为空
+        cout << "List is empty!" << endl;
+        return ElemType();  // 返回默认值0
+    }
+    ListNode* cur = head;
+    while (cur->next) {  // 遍历到最后一个节点
+        cur = cur->next;
+    }
+    return cur->val;  // 返回尾节点的值
+}
+
+// 获取指定位置的节点值
+ElemType at(ListNode* head, size_t index) {
+    ListNode* cur = head;
+    for (size_t i = 0; i < index; ++i) {  // 遍历找到目标位置的节点
+        if (!cur) {
+            cout << "Index out of bounds!" << endl;
+            return ElemType();  // 返回默认值0
         }
-        current = current->next;
+        cur = cur->next;
     }
-
-    return nullptr;  // 未找到时返回nullptr
+    return cur->val;  // 返回节点值
 }
 
-// 打印链表元素
-void printList(const List& list) {
-    const ListNode* current = list.head;
-    while (current != nullptr) {
-        cout << current->data << " ";
-        current = current->next;
+// 获取链表的大小
+size_t size(ListNode* head) {
+    size_t count = 0;
+    ListNode* cur = head;
+    while (cur) {  // 遍历链表并计数
+        ++count;
+        cur = cur->next;
     }
-    cout << endl;
-}
-
-// 判断链表是否为空
-bool isEmpty(const List& list) {
-    return list.size == 0;
-}
-
-// 获取链表大小
-size_t getSize(const List& list) {
-    return list.size;
+    return count;  // 返回链表大小
 }
 
 // 清空链表
-void clearList(List& list) {
-    while (list.head != nullptr) {
-        popFront(list);
-        // 如果用popBack效率会变慢
+void clear(ListNode*& head) {
+    while (head) {
+        pop_front(head);  // 依次删除链表头节点
     }
 }
 
-// 析构函数，释放链表内存
-void destroyList(List& list) {
-    clearList(list);
+// 打印链表
+void print(ListNode* head) {
+    ListNode* cur = head;
+    while (cur) {  // 遍历链表
+        cout << cur->val << " -> ";  // 输出节点值
+        cur = cur->next;
+    }
+    cout << "nullptr" << endl;  // 输出链表结束标志
 }
 
+// 测试代码
 int main() {
-    List myList;
-    initList(myList);
+    ListNode* head = nullptr;  // 初始化空链表
 
-    pushBack(myList, 1);
-    pushBack(myList, 2);
-    pushFront(myList, 0);
+    push_back(head, 1);  // 在尾部插入1
+    push_back(head, 2);  // 在尾部插入2
+    push_back(head, 3);  // 在尾部插入3
+    push_front(head, 0); // 在头部插入0
+    insert(head, 2, 99); // 在索引2位置插入99
+    print(head);  // 打印链表 0 -> 1 -> 99 -> 2 -> 3 -> nullptr
 
-    insert(myList, 2, 5);    // 在位置2插入元素5
-    insert(myList, 2, 6);    // 在位置2插入元素5
-    insert(myList, 2, 7);    // 在位置2插入元素5
-    insert(myList, 999, 8);  // 在位置999插入元素5
-    cout << "find next ******" << findElem(myList, 5)->next->data << "*****" << endl;
+    pop_front(head);  // 删除头节点
+    pop_back(head);   // 删除尾节点
+    erase(head, 1);   // 删除索引1位置的节点
+    print(head);  // 打印链表 1 -> 99 -> nullptr
 
-    cout << "List size: " << getSize(myList) << endl;
-    printList(myList);
+    cout << "Front: " << front(head) << ", Back: " << back(head) << endl;  // 打印头尾节点值
 
-    popFront(myList);
-    popBack(myList);
-    erase(myList, 3);    // 删除位置3的元素
-    erase(myList, 999);  // 删除位置999的元素
+    set(head, 1, 42);  // 修改索引1位置的节点值为42
+    print(head);  // 打印链表 1 -> 42 -> nullptr
 
-    cout << "List size: " << getSize(myList) << endl;
-    printList(myList);
-
-    clearList(myList);
-
-    cout << "List size: " << getSize(myList) << endl;
-
-    destroyList(myList);
+    clear(head);  // 清空链表
+    cout << "List is empty: " << boolalpha << (head == nullptr) << endl;  // 判断链表是否为空
 
     return 0;
 }
---
+++ 2-2/2-2-1_list/List_with_tail.cpp
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+typedef int ElemType;  // 数据元素类型约定为ElemType, 由用户在使用该数据类型时自行定义
+
+/*
+为什么要有链表的存在?（相比较顺序表的优势而言）
+    线性存储结构在进行中间插入和删除操作时需移动大量元素，浪费资源.
+    所以考虑链式存储的线性表：即 链表
+
+
+*/
+
+/****************************** 结构定义 ************************************/
+
+// 结点，只有后继，是单向链表
+struct ListNode {
+    ElemType data;
+    struct ListNode* next;
+};
+
+// 链表
+struct List {
+    struct ListNode* head;
+    struct ListNode* tail;
+    size_t size;
+};
+
+// 初始化链表
+void initList(List& list) {
+    list.head = list.tail = NULL;
+    list.size = 0;
+}
+
+// 创建节点
+ListNode* createNode(int value) {
+    ListNode* newNode = new ListNode;
+    if (newNode == nullptr) {
+        // 处理内存分配失败的情况
+        cerr << "Memory allocation failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    return newNode;
+}
+
+// 在链表头部插入元素
+void pushFront(List& list, int value) {
+    ListNode* newNode = createNode(value);
+
+    if (list.head == nullptr) {
+        list.head = list.tail = newNode;
+    } else {
+        newNode->next = list.head;
+        list.head = newNode;
+    }
+
+    list.size++;
+}
+
+// 在链表尾部插入元素
+void pushBack(List& list, int value) {
+    ListNode* newNode = createNode(value);
+
+    if (list.tail == nullptr) {  // 如果链表为空，那么这个结点既是头部也是尾部
+        list.head = list.tail = newNode;
+    } else {
+        list.tail->next = newNode;
+        list.tail = newNode;
+    }
+
+    list.size++;
+}
+
+// 插入元素到指定位置
+/*
+size_t 是 C/C++ 中的一种无符号整数类型，
+通常用于表示对象的大小或容器的大小。
+它是根据系统架构的不同而有所变化，但它总是足够大以容纳系统中最大可能的对象大小。
+*/
+void insert(List& list, size_t position, int value) {
+    if (position > list.size) {
+        cerr << "Invalid position for insertion" << endl;
+        return;
+    }
+
+    if (position == 0) {
+        pushFront(list, value);
+    } else if (position == list.size) {
+        pushBack(list, value);
+    } else {
+        ListNode* newNode = createNode(value);
+        ListNode* current = list.head;
+        
+        // 遍历n - 1次, 到要插入位置前一个结点处
+        for (size_t i = 1; i < position; ++i) {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+
+        list.size++;
+    }
+}
+
+// 删除链表头部元素
+void popFront(List& list) {
+    if (list.head != nullptr) {
+        ListNode* temp = list.head;
+        list.head = list.head->next;
+        delete temp;
+
+        if (list.head == nullptr) {
+            list.tail = nullptr;
+        }
+
+        list.size--;
+    }
+}
+
+// 删除链表尾部元素
+void popBack(List& list) {
+    if (list.head != nullptr) {
+        ListNode* temp = list.head;
+        ListNode* prev = nullptr;
+
+        // 单项链表只能从头找删除的前一个位置
+        while (temp->next != nullptr) {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        delete temp;
+
+        if (prev == nullptr) {
+            list.head = list.tail = nullptr;
+        } else {
+            prev->next = nullptr;
+            list.tail = prev;
+        }
+
+        list.size--;
+    }
+}
+
+// 删除链表中特定位置的节点
+void erase(List& list, size_t position) {
+    if (position >= list.size) {
+        cerr << "Invalid position for erasing" << endl;
+        return;
+    }
+
+    if (position == 0) {
+        popFront(list);
+    } else if (position == list.size - 1) {
+        popBack(list);
+    } else {
+        ListNode* current = list.head;  // 头结点的位置是0
+        ListNode* prev = nullptr;       // 当前位置的前一个结点
+
+        // 遍历n次, 到要删除位置结点处
+        for (size_t i = 0; i < position; ++i) {
+            prev = current;
+            current = current->next;
+        }
+
+        prev->next = current->next;
+        delete current;
+
+        list.size--;
+    }
+}
+
+ListNode* findElem(List& list, ElemType value) {
+    ListNode* current = list.head;
+
+    while (current != nullptr) {
+        if (current->data == value) {
+            return current;  // 返回找到的节点的指针
+        }
+        current = current->next;
+    }
+
+    return nullptr;  // 未找到时返回nullptr
+}
+
+// 打印链表元素
+void printList(const List& list) {
+    const ListNode* current = list.head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+// 判断链表是否为空
+bool isEmpty(const List& list) {
+    return list.size == 0;
+}
+
+// 获取链表大小
+size_t getSize(const List& list) {
+    return list.size;
+}
+
+// 清空链表
+void clearList(List& list) {
+    while (list.head != nullptr) {
+        popFront(list);
+        // 如果用popBack效率会变慢
+    }
+}
+
+// 析构函数，释放链表内存
+void destroyList(List& list) {
+    clearList(list);
+}
+
+int main() {
+    List myList;
+    initList(myList);
+
+    pushBack(myList, 1);
+    pushBack(myList, 2);
+    pushFront(myList, 0);
+
+    insert(myList, 2, 5);    // 在位置2插入元素5
+    insert(myList, 2, 6);    // 在位置2插入元素5
+    insert(myList, 2, 7);    // 在位置2插入元素5
+    insert(myList, 999, 8);  // 在位置999插入元素5
+    cout << "find next ******" << findElem(myList, 5)->next->data << "*****" << endl;
+
+    cout << "List size: " << getSize(myList) << endl;
+    printList(myList);
+
+    popFront(myList);
+    popBack(myList);
+    erase(myList, 3);    // 删除位置3的元素
+    erase(myList, 999);  // 删除位置999的元素
+
+    cout << "List size: " << getSize(myList) << endl;
+    printList(myList);
+
+    clearList(myList);
+
+    cout << "List size: " << getSize(myList) << endl;
+
+    destroyList(myList);
+
+    return 0;
+}
